c98048555e1df704f42f32ae1fd0a8e42a38025c
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-05-08 21:47:58 +0530

    DTO

---
+++ src/main/java/paymentswebapp/DTO/User.java
@@ -0,0 +1,42 @@
+package paymentswebapp;
+
+public class User {
+
+	String Name;
+	String Email;
+	String PhoneNum;
+	String DateOfBirth;
+	String Password;
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getPhoneNum() {
+		return PhoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		PhoneNum = phoneNum;
+	}
+	public String getDateOfBirth() {
+		return DateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		DateOfBirth = dateOfBirth;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	
+}
