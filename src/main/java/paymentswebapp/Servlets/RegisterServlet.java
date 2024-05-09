28702194a29434d4e34d52bf4eb22757ef7ed0f2
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-05-09 09:17:28 +0530

    Servlet

--- src/main/java/paymentswebapp/Servlets/RegisterServlet.java
+++ src/main/java/paymentswebapp/Servlets/RegisterServlet.java
@@ -1,5 +1,6 @@
 package paymentswebapp;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -17,21 +18,19 @@ public class RegisterServlet extends HttpServlet {
       
     }
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			
-		
+				
 	}
-
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name = request.getParameter("Firstname");
 		String email =request.getParameter("email");
 		String dob = request.getParameter("dob");
 		String phno = request.getParameter("phonenumber");
+		String address = request.getParameter("Address");
 		String pass = request.getParameter("pass");
 		
-		response.getWriter().write("Successfully Registered");
+//		response.getWriter().write("Successfully Registered");
 		
 
 		try {
@@ -40,20 +39,25 @@ public class RegisterServlet extends HttpServlet {
 			u.setName(name);
 			u.setPhoneNum(phno);
 			u.setDateOfBirth(dob);
+			u.setAddress(address);
 			u.setEmail(email);
 			u.setPassword(pass);
-			
-			
-						
 			
 			paymentsAppdao dao = new paymentsAppdao();
 			dao.RegisterDetails(u);
 			
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
+			
 			e.printStackTrace();
+			
 		}
-	
+		response.setContentType("text/html");
+//		response.getWriter().write("null");
+		RequestDispatcher rd = request.getRequestDispatcher("RegistrationSuccessfull.jsp");
+		rd.include(request, response);
+		
 	}
-	}
+}
 
 
