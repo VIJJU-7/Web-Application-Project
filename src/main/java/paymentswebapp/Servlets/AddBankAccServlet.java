226c46f0e71fea12b1b5eff054257aa42df1537f
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-05-09 09:17:19 +0530

    Servlet

---
+++ src/main/java/paymentswebapp/Servlets/AddBankAccServlet.java
@@ -0,0 +1,37 @@
+package paymentswebapp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class AddBankAccServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public AddBankAccServlet() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+//		String BaName = request.getParameter("AccNum");
+//		String BaName = request.getParameter("AccNum");
+//		String BaName = request.getParameter("AccNum");
+//		String BaName = request.getParameter("AccNum");
+//		String BaName = request.getParameter("AccNum");
+		
+		
+	}
+
+}
