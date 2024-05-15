b53672b41552f0bb5378d81c2bc089348bae55db
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-05-15 21:34:21 +0530

    JS

---
+++ src/main/webapp/JS/AddBankAccount.js
@@ -0,0 +1,33 @@
+window.addEventListener("DOMContentLoaded", function() {
+  let signUpButton = document.querySelector("#a-form .submit");
+
+  let checkInputFields = (e) => {
+    let inputs = document.querySelectorAll('#a-form .form__input');
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].value.trim() === '') {
+        e.preventDefault();
+        alert('Some Input fields cannot be empty');
+        return;
+      }
+    }
+  }
+  let backButton = document.querySelector("#back-button");
+
+let redirectToPreviousPage = (e) => {
+    e.preventDefault();
+    window.location.href = "AddBankAccount.jsp";
+}
+
+backButton.addEventListener("click", redirectToPreviousPage);
+
+
+  signUpButton.addEventListener("click", checkInputFields);
+  signUpButton.addEventListener("click", checkInputFields);
+  backButton.addEventListener("click", function() {
+    window.location.href = "BankAccount.jsp";
+  });
+});
+
+
+
+
