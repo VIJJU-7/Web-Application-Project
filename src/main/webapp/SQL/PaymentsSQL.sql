8158c0b85d8bb8a37a5f4337232af7161a89385e
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-05-15 21:33:47 +0530

    SQL

--- src/main/webapp/SQL/PaymentsSQL.sql
+++ src/main/webapp/SQL/PaymentsSQL.sql
@@ -21,7 +21,7 @@ BaType                enum ("CURRENT", "LOAN", "SAVINGS", "SALARY"),
 BaTypeID              int not null,
 ACCT_Type_Code        varchar(3),
 ACCT_Type_Description varchar(25),
-primary key           (Bank_ACCT_Type_ID)
+primary key           (BaTypeID)
 );
 
 select * from Bank_Account_Types;
@@ -34,7 +34,7 @@ desc Bank_Account_Types;
 create table Bank_Acct_Details(
 User_ID           int not null,
 BaTypeID          int not null,
-BaNumber          long not null,
+BaNumber          varchar(25) not null,
 BaName            varchar(20),
 IFSC_CODE         varchar(10),
 BaPin             varchar(10),
@@ -45,6 +45,8 @@ references        Bank_Account_Types (BaTypeID),
 foreign key      (User_ID)
 references        Register_Details (User_ID)
 );
+
+select * from Bank_Acct_Details;
 
 desc Bank_Acct_Details;
 
@@ -59,7 +61,10 @@ alter table Register_Details add Address varchar(50) not null;
 
 alter table Register_Details add User_ID int primary key auto_increment not null;
 
+
 alter table Register_Details modify column Phone_Num bigint unique not null;
+alter table Bank_Acct_Details modify column User_ID int null;
+alter table Bank_Acct_Details modify column BaTypeID int null;
 
 set sql_safe_updates = 0;
 set sql_safe_updates = 1;
