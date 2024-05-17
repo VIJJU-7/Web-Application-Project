acd4f2a9ee074305a64b1cd34258a19b2b2bb08e
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-05-17 09:12:15 +0530

    DAO

--- src/main/java/paymentswebapp/DAO/paymentsAppdao.java
+++ src/main/java/paymentswebapp/DAO/paymentsAppdao.java
@@ -2,8 +2,12 @@ package paymentswebapp;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class paymentsAppdao {
 	
@@ -13,7 +17,8 @@ public void RegisterDetails(User u) {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/vijay","root","Vijay@7777");
 		Statement stmt = con.createStatement();
-		String query = "Insert into Register_Details (Register_Name, Phone_Num, Email, Address, DOB, Password) values ('"+u.getName()+"','"+u.getPhoneNum()+"','"+u.getEmail()+"','"+u.getAddress()+"','"+u.getDateOfBirth()+"','"+u.getPassword()+"')";
+		String query = "Insert into Register_Details (Register_Name, Phone_Num, Email, Address, DOB, Password) "
+				       + "values ('"+u.getName()+"','"+u.getPhoneNum()+"','"+u.getEmail()+"','"+u.getAddress()+"','"+u.getDateOfBirth()+"','"+u.getPassword()+"')";
 		stmt.executeUpdate(query);
 		stmt.close();
 		con.close();
@@ -44,43 +49,45 @@ public void RegisterDetails(User u) {
 	return false;
 }
 
-
-
+}
+//
 //	public void addBankAccount(BankAccount b) {
+//		
 //	    try {
 //	        Class.forName("com.mysql.cj.jdbc.Driver");
 //	        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/vijay","root","Vijay@7777");
 //	        Statement stmt = con.createStatement();
-//	        String query = "INSERT INTO Bank_Acct_Details (User_ID, BaTypeID, BaName, BaNumber, IFSC_CODE, Phonenum, BaAccName, CurrentBankBal) VALUES ('"+b.getUserId()+"','"+b.getBaTypeID()+"','"+b.getBaName()+"','"+b.getBaNumber()+"','"+b.getIFSC_CODE()+"','"+b.getPhonenum()+"','"+b.getBaAccName()+"','"+b.getCurrBankBal()+"')";
-//	        stmt.executeUpdate(query);
-//	        stmt.close();
-//	        con.close();
-//	    } catch (Exception e){
-//	        e.printStackTrace();
+//	        
+//	        String query = "insert into Bank_acct_details(Acct_Num, Acct_Holder_Name, Bank_Name, Acct_IFSC_Code, Acct_Type, User_Id, BanK_Balance, Phone_Num)"
+//					       + "values('" + b.getBankAcctNum() + "','" + b.getBankAcctHolderName() + "', '" + b.getBankName()+ "', '" + b.getBankIfscCode() + "',"
+//					       	+ " '"+ b.getBankAcctType()+ "', '" + b.getUserId() + "', '" + b.getBankAcctCurBalance() + "','" + b.getPhoneNumber()+ "')";
+//	       
+//	        int rs = stmt.executeUpdate(query);
+//	    
+//	    
 //	    }
+//	        public List<BankAccount> BaList(int UserID) throws SQLException{
+//	    		
+//	    		List<BankAccount> balist = new ArrayList<BankAccount>();
+//	    		
+//	    		String Query = "Select Acct_Num,Acct_Holder_Name,Acct_IFSC_Code,BanK_Balance from BankAccount Where UserId=?";
+//	    				
+//	    		Connection con;
+//				PreparedStatement st = con.prepareStatement(Query);
+//	    		st.setInt(1, UserID);
+//	    		
+//	    		ResultSet rs = st.executeQuery();
+//	    		while(rs.next()) {
+//	    			BankAccount b = new BankAccount();
+//	    			b.setBankAcctNum(rs.getString("BankAcctNo"));
+//	    			b.setBankAcctHolderName(rs.getString("BankAcctName"));
+//	    			b.setBankIfscCode(rs.getString("BankIFSCCode"));
+//	    			b.setBankAcctCurBalance(rs.getDouble("BanK_Balance"));
+//	    			b.setBankAcctPin(rs.getString("BankAcctPin"));
+//	    			balist.add(b);
+//	    		}
+//	    		return balist;
+//	        }
+//	}
 	
-	public void addBankAccount(BankAccount b) {
-	    try {
-	        Class.forName("com.mysql.cj.jdbc.Driver");
-	        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/vijay","root","Vijay@7777");
-	        Statement stmt = con.createStatement();
-
-	       
-	        String checkQuery = "SELECT * FROM Bank_Account_Types WHERE BaTypeID = '" + b.getBaTypeID() + "'";
-	        ResultSet rs = stmt.executeQuery(checkQuery);
-
-	        if (!rs.next()) {
-	            System.out.println("Error: BaTypeID does not exist in bank_account_types.");
-	            return;
-	        }
-
-	        String query = "INSERT INTO Bank_Acct_Details (User_ID, BaTypeID, BaName, BaNumber, IFSC_CODE, Phonenum, BaAccName, CurrentBankBal) VALUES ('"+b.getUserId()+"','"+b.getBaTypeID()+"','"+b.getBaName()+"','"+b.getBaNumber()+"','"+b.getIFSC_CODE()+"','"+b.getPhonenum()+"','"+b.getBaAccName()+"','"+b.getCurrBankBal()+"')";
-	        stmt.executeUpdate(query);
-	        stmt.close();
-	        con.close();
-	    } catch (Exception e){
-	        e.printStackTrace();
-	    }
-	}
-
-	}
+	
