7a7d2014ee615c5e6913ab6d6f5c68f93d80e7d7
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-05-17 09:12:34 +0530

    SERVLET

--- src/main/java/paymentswebapp/Servlets/AddBankAccServlet.java
+++ src/main/java/paymentswebapp/Servlets/AddBankAccServlet.java
@@ -1,36 +1,93 @@
 package paymentswebapp;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class AddBankAccServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        String accountHolderName = request.getParameter("Name");
-        String accountNumber = request.getParameter("AccNum");
-        String ifsc = request.getParameter("IFSC");
-        String branch = request.getParameter("Branch");
-        
-       
-        BankAccount ba = new BankAccount();
-        ba.setBaName(accountHolderName);
-        ba.setBaNumber(accountNumber);
-        ba.setIFSC_CODE(ifsc);
-        ba.setBaBranch(branch);
-        ba.setBaTypeID(0);
-        ba.setCurrBankBal(1000);
-        ba.setUserId(0);
-        
-
-        paymentsAppdao dao = new paymentsAppdao();
-        dao.addBankAccount(ba);
-        response.sendRedirect("Dashboard.jsp");
+    public AddBankAccServlet() {
+    	super();
     }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//    	String accountNumber = request.getParameter("AccNum");
+//		String acctHolderName = request.getParameter("Name");
+//		String bankName = request.getParameter("BaName");
+//		String bankIFSCCode = request.getParameter("IFSC");
+//		String bankAcctType = request.getParameter("paymentOption");
+//		String phoneNumber = request.getParameter("phonenum");
+//		String bankAcctPin = request.getParameter("bankAcctPin");
+//		String userName = request.getParameter("username");
+//		
+//		
+//		paymentsAppdao dao = new paymentsAppdao();
+////		User u = new User();
+////		int currUserId = u.getUserId();
+//		
+//		BankAccount ba = new BankAccount();
+//		
+//		ba.setBankAcctNum(accountNumber);
+//		ba.setBankAcctHolderName(acctHolderName);
+//		ba.setBankName(bankName);
+//		ba.setBankIfscCode(bankIFSCCode);
+//		ba.setBankAcctType(bankAcctType);
+//		ba.setPhoneNumber(phoneNumber);
+////		ba.setBankAcctPin(bankAcctPin);
+////		ba.setUserId(currUserId);
+//	
+//		try {
+//			dao.addBankAccount(ba);
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//		response.setContentType("text/html");
+////		response.getWriter().write("<p style='color:green;'>Account Added Successful!<p>");
+//		RequestDispatcher rd = request.getRequestDispatcher("/Dashboard.jsp");
+//		rd.include(request, response);
+//    	
+//    }
+//}
+    	String accountNumber = request.getParameter("AccNum");
+		String acctHolderName = request.getParameter("Name");
+		String bankName = request.getParameter("BaName");
+		String bankIFSCCode = request.getParameter("IFSC");
+		String bankAcctType = request.getParameter("paymentOption");
+		String phoneNumber = request.getParameter("phonenum");
+		String bankAcctPin = request.getParameter("bankAcctPin");
+		String userName = request.getParameter("username");
+		
+		BankAccount ba = new BankAccount();
+		ba.setBankAcctNum(accountNumber);
+		ba.setBankAcctHolderName(acctHolderName);
+		ba.setBankName(bankName);
+		ba.setBankIfscCode(bankIFSCCode);
+		ba.setBankAcctType(bankAcctType);
+		ba.setPhoneNumber(phoneNumber);
+		
+		try {
+BankAccountdao db = new BankAccountdao();
+
+			if(db.addBankAcct(ba)>0) {
+				RequestDispatcher rd = request.getRequestDispatcher("Dashboard.jsp");
+				rd.forward(request, response);		
+			}
+			else {
+				response.setContentType("text/html");
+				RequestDispatcher rd = request.getRequestDispatcher("AddBankAccount.jsp");
+				rd.include(request, response);
+			}
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+}
 }
